56a8307496ee1dd5f4a6ae6072725364f8f0cb2f
codeflying0817 <149983858+flycoding0817@users.noreply.github.com>
2024-01-13 15:50:32 +0800

    Add files via upload

---
+++ ListNode.java
@@ -0,0 +1,10 @@
+package leedCode;
+
+public class ListNode {
+	 int val;
+	    ListNode next;
+	 
+	    ListNode(int x){
+	        val = x;
+	    }
+}
---
+++ num_1.java
@@ -0,0 +1,27 @@
+package leedCode;
+import java.util.*;
+
+
+public class num_1 {
+
+
+	public static int[] twoSum(int[] nums, int target) {
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = i+1; j < nums.length; j++) {
+				if (nums[i]+nums[j] == target) {
+					return new int[] {i,j};
+				}
+			}
+			
+		}
+		throw new IllegalArgumentException("NO solution");
+        
+    }
+	
+	public static void main(String[] args) {
+		int[] arr = {3,2,4};
+		int[] aas = twoSum(arr,6);
+		System.out.println(Arrays.toString(aas));
+	}
+}
+
